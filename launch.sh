f58b2c88401bc4faf9eebf1e5630af7ec3be4475
Ene-Daniele <ene.daniele@iisgalvanimi.edu.it>
2024-03-12 10:48:55 +0000

    rimosso compilazioni a js

--- launch.sh
+++ launch.sh
@@ -3,8 +3,6 @@
 # ⚠️ [CTRL]+[C] WILL NOT BUILD THE PROJECT ⚠️
 
 echo "[LAUNCHER] Launching from: root/launch.sh"
-echo "[LAUNCHER] Compiling TypeScript..."
-tsc src/main.ts --outDir src/build/
 echo "[LAUNCHER] Running Vite..."
 echo "[LAUNCHER] Use [Q]+[ENTER] to quit safely and allow vite to build the project!"
 npx vite
