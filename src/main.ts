fa75258980ce72f2b8eb0290b2ca626da8737197
Ene-Daniele <ene.daniele@iisgalvanimi.edu.it>
2024-03-12 12:55:08 +0000

    THIS RIGHT HERE IS AS FAR AS YOU GOOOOO THIS RIGHT HERE IS THE END OF THE ROAAAAAAAD this right here is as far as you GOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO SO BURN IT ALL DOWN BURN IT ALL ODWN I DONT GIVE A UCK BURN IT ALL DOWN BURN IT TO THE GROUNJD

--- src/main.ts
+++ src/main.ts
@@ -1,29 +1,43 @@
 import * as THREE from 'three';
 
+//! IMPORT HELPER ENTITIES
 //! SETUP
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 
-const renderer = new THREE.WebGLRenderer();
+//@ts-ignore
+const renderer = new THREE.WebGLRenderer({canvas: artifactCanvas});
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
 
-const geometry = new THREE.BoxGeometry( 1, 1, 1 );
-const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-const cube = new THREE.Mesh( geometry, material );
-scene.add( cube );
 
 //@ts-ignore
-camera.position.z = 2;
+camera.position.z = 10;
 
-
+enum BuildingType {
+	SolarPanel = 0,
+	House = 1,
+}
+interface Vector3D {
+	x: number;
+	y: number;
+	z: number;
+}
+interface Building {
+	type: BuildingType;
+	position: Vector3D;
+	size: Vector3D;
+}
+function buildEntity(building: Building) {
+	
+	const geometry = new THREE.BoxGeometry(building.position.x, building.position.y, building.position.z);
+	const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+	const cube = new THREE.Mesh( geometry, material );
+	scene.add( cube );
+}
 //! ANIMATION LOOP
 function animate() {
 	requestAnimationFrame( animate );
-
-	cube.rotateX(0.01);
-	cube.rotateY(0.10);
-
 	renderer.render( scene, camera );
 }
 
